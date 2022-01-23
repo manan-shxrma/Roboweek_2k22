1c7263d98ffa21e73915e0aac36a1bf7b0060289
manan-shxrma <manansharmams3@gmail.com>
2022-01-23 10:41:15 +0530

    testing

--- src/js/sponsersdata.js
+++ src/js/sponsersdata.js
@@ -15,4 +15,8 @@ export const GoldSponsors = [
         sponsorLink: "https://github.com/",
         sponsorImg: "/assets/images/logos/GitHub_Logo.png",
     },
+    {
+        sponsorLink: "https://github.com/",
+        sponsorImg: "/assets/images/logos/GitHub_Logo.png",
+    }
 ]
