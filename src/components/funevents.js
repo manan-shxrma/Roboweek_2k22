049240d2ba60e1bfcb98df6bacc0cec2569fae41
Mridul07Sharma <mridul07sharma@gmail.com>
2022-02-03 10:04:21 +0530

    kkk

--- src/components/funevents.js
+++ src/components/funevents.js
@@ -6,6 +6,7 @@ import AOS from 'aos';
 AOS.init();
 
 function Services(){
+    
     return(<><div className="container mainbodyoffuneve">
     
     <div data-aos="fade-right"
@@ -28,7 +29,7 @@ function Services(){
 		<div class="col-md-4">
     
     <div  data-aos="fade-right" class="profile-card-4 text-center"><img style={{height:"27vh"}} alt="" src="assets/images/trash-bot.jpeg" class="img img-responsive" />
-        <div style={{backgroundColor:"rgb(255,182,193)"}} class="profile-content">
+        <div style={{backgroundColor:"rgb(255,182,193)", paddingBottom:"4vh"}} class="profile-content">
         <span class="tag12 ">Technical Creativity</span>
             <div class="profile-name">TRASH BOT</div>
             <div><b>TECH EVENT</b></div>
@@ -41,7 +42,7 @@ function Services(){
 <div class="col-md-4">
     
     <div data-aos="fade-up" class="profile-card-4 text-center"><img style={{height:"27vh"}} alt="" src="assets/images/code-nari.jpeg" class="img img-responsive" />
-        <div style={{backgroundColor:"rgb(255,182,193)"}}  class="profile-content">
+        <div style={{backgroundColor:"rgb(255,182,193)", paddingBottom:"4vh"}}  class="profile-content">
         <span class="tag12 ">Coding contest</span>  
            
             <div class="profile-name">CODE NAARI </div>
@@ -55,7 +56,7 @@ function Services(){
 <div class="col-md-4">
     
     <div data-aos="fade-left" class="profile-card-4 text-center"><img style={{height:"27vh"}} alt="" src="assets/images/callisto.jpg" class="img img-responsive" />
-        <div style={{backgroundColor:"rgb(255,182,193)"}} class="profile-content">
+        <div style={{backgroundColor:"rgb(255,182,193)", paddingBottom:"4vh"}} class="profile-content">
         <span class="tag12 ">Technical Writing</span>
             <div class="profile-name">CALLISTO
                 
@@ -71,13 +72,13 @@ function Services(){
     <div style={{paddingBottom:"5%"}} class="col-md-4">
     
     <div data-aos="fade-right" class="profile-card-4 text-center"><img style={{height:"27vh"}} alt="" src="assets/images/hunt-nova.jpg" class="img img-responsive" />
-        <div style={{backgroundColor:"rgb(255,182,193)"}} class="profile-content">
+        <div style={{backgroundColor:"rgb(255,182,193)", paddingBottom:"4vh"}} class="profile-content">
         <span class="tag12 ">Deduction</span>
             <div class="profile-name">HUNT NOVA
                 
             </div>
             <div><b>TECH EVENT</b></div>
-            <div class="profile-description">Play with your logic to crack the components and build a working prototype on TinkerCad.
+            <div class="profile-description mb-2">Play with your logic to crack the components and build a working prototype on TinkerCad.
 </div>
             <a href="/" class="bn5">Register</a><Link to={`/post/event4`} class="bn5">Details</Link>
         </div>
@@ -85,52 +86,52 @@ function Services(){
     <div class="col-md-4">
     
     <div data-aos="fade-up" class="profile-card-4 text-center"><img style={{height:"27vh"}} alt="" src="assets/images/code-play.png" class="img img-responsive" />
-        <div style={{backgroundColor:"rgb(255,182,193)"}} class="profile-content">
+        <div style={{backgroundColor:"rgb(255,182,193)", paddingBottom:"4vh"}} class="profile-content">
         <span class="tag12 ">Coding</span>
             <div class="profile-name">CODE PLAY
                 
             </div>
             <div><b>TECH EVENT</b></div>
-            <div class="profile-description">Compete with many coders from around the country.</div>
+            <div class="profile-description mb-2">Compete with many coders from around the country.</div>
             <a href="/" class="bn5">Register</a><Link to={`/post/event7`} class="bn5">Details</Link>
         </div>
     </div></div>
     <div class="col-md-4">
     
     <div data-aos="fade-left" class="profile-card-4 text-center"><img style={{height:"27vh"}} alt="" src="assets/images/design-wars.png" class="img img-responsive" />
-        <div style={{backgroundColor:"rgb(255,182,193)"}} class="profile-content">
+        <div style={{backgroundColor:"rgb(255,182,193)", paddingBottom:"4vh"}} class="profile-content">
         <span class="tag12 ">Mech. Design</span>
             <div class="profile-name">DESIGN WARS
                 
             </div>
             <div><b>TECH EVENT</b></div>
-            <div class="profile-description">-Design Wars 2.0  aims to inspire learning through a real-world scenario. </div>
+            <div class="profile-description mb-2">-Design Wars 2.0  aims to inspire learning through a real-world scenario. </div>
             <a href="/" class="bn5">Register</a><Link to={`/post/event8`} class="bn5">Details</Link>
         </div>
     </div></div>
     <div style={{paddingBottom:"5%"}} class="col-md-4">
     
     <div data-aos="fade-right" class="profile-card-4 text-center"><img alt="" style={{height:"27vh"}} src="assets/images/REGATTA.png" class="img img-responsive" />
-        <div style={{backgroundColor:"rgb(255,182,193)"}} class="profile-content">
+        <div style={{backgroundColor:"rgb(255,182,193)", paddingBottom:"4vh"}} class="profile-content">
         <span class="tag12 ">ROS</span>
             <div class="profile-name">REGATTA 2.0
                
             </div>
             <div><b>TECH EVENT</b></div>
-            <div class="profile-description">Solve the problem statements using ROS NOETIC, ROS MELODIC or constructsim(online).</div>
+            <div class="profile-description mb-2">Solve the problem statements using ROS NOETIC, ROS MELODIC or constructsim(online).</div>
             <a href="/" class="bn5">Register</a><Link to={`/post/event9`} class="bn5">Details</Link>
         </div>
     </div></div>
     <div class="col-md-4">
     
     <div data-aos="fade-up" class="profile-card-4 text-center"><img style={{height:"27vh"}} alt="" src="assets/images/clipped.jpg" class="img img-responsive" />
-        <div style={{backgroundColor:"rgb(230, 255, 110)"}} class="profile-content">
+        <div style={{backgroundColor:"rgb(230, 255, 110)", paddingBottom:"4vh"}} class="profile-content">
         <span class="tag12 ">Graphical Designing</span>
             <div class="profile-name">#CLIPPED
               
             </div>
             <div><b>FUN EVENT</b></div>
-            <div class="profile-description">Enable you to put your creative and editing skills at one place
+            <div class="profile-description mb-2">Enable you to put your creative and editing skills at one place
 </div>
             <a href="/" class="bn5">Register</a><Link to={`/post/event5`} class="bn5">Details</Link>
         </div>
@@ -138,13 +139,13 @@ function Services(){
     <div class="col-md-4">
     
     <div data-aos="fade-left" class="profile-card-4 text-center"><img style={{height:"27vh"}} alt="" src="assets/images/Arcanum.png" class="img img-responsive" />
-        <div style={{backgroundColor:"rgb(230, 255, 110)"}} class="profile-content">
+        <div style={{backgroundColor:"rgb(230, 255, 110)", paddingBottom:"4vh"}} class="profile-content">
         <span class="tag12 ">Deduction</span>
             <div class="profile-name">
                ARCANUM
             </div>
             <div><b>FUN EVENT</b></div>
-            <div class="profile-description">A virtual treasure hunt- let the mystery saga unfold.</div>
+            <div class="profile-description mb-2">A virtual treasure hunt- let the mystery saga unfold.</div>
             <a href="/" class="bn5">Register</a><Link to={`/post/event6`} class="bn5">Details</Link>
         </div>
     </div></div>
