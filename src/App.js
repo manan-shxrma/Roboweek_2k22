4e0d96e2f1070b487fa7602924879317ddd1c049
Baibhav <53977165+baibhav3211@users.noreply.github.com>
2022-02-06 19:43:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
 
 
         // `https://roboweek.netlify.app/` 
-        `http://www.roboweek-robosocnith.com/` 
+        `www.roboweek-robosocnith.com/` 
 
       //  'http://localhost:3000/'
         //change this url when using localhost to http://localhost:3000/
