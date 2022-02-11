5fb68ca66741b07345f109afbb66a47564287358
Abhishek <kentdavid070@gmial.com>
2022-02-11 15:47:51 +0530

    code play

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
         // `https://roboweek.netlify.app/` 
         `https://roboweek-robosocnith.com/` 
 
-      //  'http://localhost:3000/'
+      // 'http://localhost:3000/'
         //change this url when using localhost to http://localhost:3000/
  
         )
