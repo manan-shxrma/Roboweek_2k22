5d01fa372da2ccb02b3574867a95fd81a4eab4f9
Arjun Thakur <21bme057@nith.ac.in>
2022-02-17 10:38:29 +0530

    competetion footer

--- src/components/aboutsection.js
+++ src/components/aboutsection.js
@@ -8,8 +8,6 @@ AOS.init();
 
 
 function Aboutsection(){
-    
-
     return(<>
     
 
@@ -30,73 +28,67 @@ function Aboutsection(){
                   <h6>About Us</h6>
                   <h4>What is  <em>Roboweek?</em></h4>
                   <div className="line-dec"></div>
-                </div><p style={{fontSize:"larger;"}}><strong> It is a blend of affairs encompassing week long enticing activities
-                          ranging from guest talks, robotic workshops, coding manias, fun events, cryptic hunt,
-                          quizzes and much more.</strong>
-                          <h4 style={{color:"#f7931e;"}}
-
+                </div><p style={{fontSize:"larger"}}><strong> It is a blend of affairs encompassing week long enticing activities
+                              ranging from guest talks, robotic workshops, coding manias, fun events, cryptic hunt,
+                              quizzes and much more.</strong>
+                              <h4 style={{color:"#f7931e"}}
+                              
                                 className="py-4" style={{color:"#f58b56"}}>Previous Year Records</h4></p>
-                        <div className="row">
-                          <div className="col-lg-4 col-sm-4">
-                            <div data-aos="fade-left" className="row">
-                              <div className="skill-item third-skill-item wow fadeIn" data-wow-duration="1s" data-wow-delay="0s">
-                                <div className="progress" data-percentage="100">
-                                  <span className="progress-left">
-                                    <span className="progress-bar"></span>
-                                  </span>
-                                  <span className="progress-right">
-                                    <span className="progress-bar"></span>
-                                  </span>
-                                  <div className="progress-value">
-                                    <div>
-                                      3000+<br />
-                                      <span>Registrations</span>
-                                    </div>
-                                  </div>
-                                </div>
-                              </div>
-                            </div>
-                            <div className="col-lg-4 col-sm-4">
-                            <div data-aos="fade-up" className="row">
-                              <div className="skill-item second-skill-item wow fadeIn" data-wow-duration="1s" data-wow-delay="0s">
-                                <div className="progress" data-percentage="100">
-                                  <span className="progress-left">
-                                    <span className="progress-bar"></span>
-                                  </span>
-                                  <span className="progress-right">
-                                    <span className="progress-bar"></span>
-                                  </span>
-                                  <div className="progress-value">
-                                    <div>300+
-                                      <br />
-                                      <span>Colleges</span>
-                                    </div>
-                                  </div>
-                                </div>
-                              </div>
-                              </div>
-                            </div>
-                            <div className="col-lg-4 col-sm-4">
-                            <div data-aos="fade-right" className="row">
-                              <div className="skill-item first-skill-item wow fadeIn" data-wow-duration="1s" data-wow-delay="0s">
-                                <div className="progress" data-percentage="100">
-                                  <span className="progress-left">
-                                    <span className="progress-bar"></span>
-                                  </span>
-                                  <span className="progress-right">
-                                    <span className="progress-bar"></span>
-                                  </span>
-                                  <div className="progress-value">
-                                    <div>
-                                      25<br />
-                                      <span>Sponsors</span>
-                                    </div>
-                                  </div>
-                                </div>
-                              </div>
-                            </div>
-                            </div>
-                </div>
+                <div data-aos="fade-left" className="row">
+                  <div className="col-lg-4 col-sm-4">
+                    <div className="skill-item third-skill-item wow fadeIn" data-wow-duration="1s" data-wow-delay="0s">
+                      <div className="progress" data-percentage="100">
+                        <span className="progress-left">
+                          <span className="progress-bar"></span>
+                        </span>
+                        <span className="progress-right">
+                          <span className="progress-bar"></span>
+                        </span>
+                        <div className="progress-value">
+                          <div>
+                            3000+<br />
+                            <span>Registrations</span>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-lg-4 col-sm-4">
+                    <div className="skill-item second-skill-item wow fadeIn" data-wow-duration="1s" data-wow-delay="0s">
+                      <div className="progress" data-percentage="100">
+                        <span className="progress-left">
+                          <span className="progress-bar"></span>
+                        </span>
+                        <span className="progress-right">
+                          <span className="progress-bar"></span>
+                        </span>
+                        <div className="progress-value">
+                          <div>300+
+                            <br />
+                            <span>Colleges</span>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-lg-4 col-sm-4">
+                    <div className="skill-item first-skill-item wow fadeIn" data-wow-duration="1s" data-wow-delay="0s">
+                      <div className="progress" data-percentage="100">
+                        <span className="progress-left">
+                          <span className="progress-bar"></span>
+                        </span>
+                        <span className="progress-right">
+                          <span className="progress-bar"></span>
+                        </span>
+                        <div className="progress-value">
+                          <div>
+                            25<br />
+                            <span>Sponsors</span>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div> 
               </div>
             </div>
@@ -110,7 +102,7 @@ function Aboutsection(){
               
                     <img src="assets/images/diversity.jpg" className="imgabout" alt="visit"/>
                     
-              
+               
             </div>
             <div  className="col-lg-6 align-self-center  wow fadeInRight" data-wow-duration="1s" data-wow-delay="0.5s">
               <div className="about-right-content">
@@ -126,27 +118,8 @@ function Aboutsection(){
                         </p>
                         <b>
                             <p class="quote"><strong>"</strong>
-                                WOMEN ARE BOTH TALENTED AND INNOVATIVE THINKERS AND TEND TO USE SCIENCE TO LOOK FOR SOLUTIONS.<strong>"</strong> <br/> Kimberly Bryant </p>
+                                 WOMEN ARE BOTH TALENTED AND INNOVATIVE THINKERS AND TEND TO USE SCIENCE TO LOOK FOR SOLUTIONS.<strong>"</strong> <br/> Kimberly Bryant </p>
                         </b>
-
-                <div className="row">
-                  
-                  <div className="col-lg-6 col-sm-6">
-                    <div data-aos="fade-right">
-                      <div className="skill-item first-skill-item wow fadeIn" data-wow-duration="1s" data-wow-delay="0s">
-                        <div className="progress" data-percentage="100">
-                          <span className="progress-left">
-                            <span className="progress-bar"></span>
-                          </span>
-                          <span className="progress-right">
-                            <span className="progress-bar"></span>
-                          </span>
-                          <div className="progress-value">
-                            <div>
-                              700+<br/>
-                              <span>Girl Participants</span>
-                            </div>
-
                 <div data-aos="fade-left" className="row">
                   <div className="col-lg-6 col-sm-4">
                     <div className="skill-item first-skill-item wow fadeIn" data-wow-duration="1s" data-wow-delay="0s">
@@ -161,29 +134,11 @@ function Aboutsection(){
                           <div>
                             700+<br />
                             <span>Girl Participants</span>
-
                           </div>
                         </div>
                       </div>
                     </div>
                   </div>
-                  
-                  <div className="col-lg-6 col-sm-6">
-                    <div data-aos="fade-left">
-                      <div className="skill-item second-skill-item wow fadeIn" data-wow-duration="1s" data-wow-delay="0s">
-                        <div className="progress" data-percentage="100">
-                          <span className="progress-left">
-                            <span className="progress-bar"></span>
-                          </span>
-                          <span className="progress-right">
-                            <span className="progress-bar"></span>
-                          </span>
-                          <div className="progress-value">
-                            <div>9
-                              <br />
-                              <span>Community Partners</span>
-                            </div>
-
                   <div className="col-lg-6 col-sm-4">
                     <div className="skill-item second-skill-item wow fadeIn" data-wow-duration="1s" data-wow-delay="0s">
                       <div className="progress" data-percentage="100">
@@ -197,7 +152,6 @@ function Aboutsection(){
                           <div>9
                             <br />
                             <span>Community Partners</span>
-
                           </div>
                         </div>
                       </div>
@@ -220,20 +174,16 @@ function Aboutsection(){
                         </div>
                       </div>
                     </div>
-                  </div> */} 
+                  </div> */}
                 </div> 
               </div>
             </div>
-          
+            
           </div>
         </div>
       </div>
     </div>
-    </div>
-    </>)
-
-
-  }
-
+  </div></>)
+}
 
 export default Aboutsection
