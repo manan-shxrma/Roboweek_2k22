fd10cc9ba13ec5000cbf2a6d373ee94b1ffafef4
Aakash Tiwari <aakashtiwari07.12.2003@gmail.com>
2022-02-23 18:59:24 +0530

    sponsors

--- src/components/sponsors.js
+++ src/components/sponsors.js
@@ -1,5 +1,5 @@
 import React from "react"
-import {registerItems,timelineItems,prizeItems,GoldSponsors,PlatformPartners,Sponsors, CommunityPartners,FAQs,EduPartners} from '../js/sponsersdata'
+import {registerItems,timelineItems,prizeItems,GoldSponsors,PlatformPartners,Sponsors,CommunityPartners,Partners ,FAQs,EduPartners,SilverSponsers,BronzeSponsers} from '../js/sponsersdata'
 import "../css/sponsers.scss"
 import "../css/base.css"
 import AOS from 'aos';
@@ -20,16 +20,19 @@ function portfolio(){
                     <section className="sponsors-section">
                   <div className="body-content sponsors-conatiner">
 
+
+                      {/* <----------Gold- Sponsers------> */}
+
                   <div className="community">
                            
-                           {/* <div className="section-heading wow fadeInLeft" data-wow-duration="1s" data-wow-delay="0.3s">
+                           <div className="section-heading wow fadeInLeft" data-wow-duration="1s" data-wow-delay="0.3s">
                              
-                             <h4 style={{color:"#0277BD"}}>Education Partners</h4>  
-                           </div> */}
+                             <h4 class="sponsor-h4" style={{backgroundColor:"#EEBC1D",color:"#fff",textAlign:"center",marginTop:"20px",marginBottom:"20px"}}>Gold Sponsors</h4>  
+                           </div>
                                        
                                       
                                            <div className="row row-img grid" style={{"justify-content": "center"}}>
-                                               {EduPartners.map((item, index) => {
+                                               {GoldSponsors.map((item, index) => {
                                                return (
                                                    <div data-aos="fade-up" style={{paddingTop:"7%"}} className="col-md-4 col-sm-6 img-div">
                                                        <a rel="noreferrer" href={item.sponsorLink} target="_blank">
@@ -40,13 +43,178 @@ function portfolio(){
                                                })}
                                            </div>
                                        
-                                   </div>
+                        </div>
+
+                        {/* <----------Silver- Sponsers------> */}
+                  <div className="community">
+                           
+                           <div className="section-heading wow fadeInLeft" data-wow-duration="1s" data-wow-delay="0.3s">
+                             
+                             <h4 class="sponsor-h4" style={{backgroundColor:"#BCBEC0",color:"#fff",textAlign:"center",marginTop:"20px",marginBottom:"20px"}}>Silver Sponsors</h4>  
+                           </div>
+                                       
+                                      
+                                           <div className="row row-img grid" style={{"justify-content": "center"}}>
+                                               {SilverSponsers.map((item, index) => {
+                                               return (
+                                                   <div data-aos="fade-up" style={{paddingTop:"7%"}} className="col-md-4 col-sm-6 img-div">
+                                                       <a rel="noreferrer" href={item.sponsorLink} target="_blank">
+                                                           <img alt="" className="sponsor-img" src={item.sponsorImg} />
+                                                       </a>
+                                                   </div>                                                
+                                               )
+                                               })}
+                                           </div>
+                                       
+                        </div>
+
+                        {/* <----------Bronze- Sponsers------> */}
+                  <div className="community">
+                           
+                           <div className="section-heading wow fadeInLeft" data-wow-duration="1s" data-wow-delay="0.3s">
+                             
+                             <h4 class="sponsor-h4" style={{backgroundColor:"#cd7f32",color:"#fff",textAlign:"center",marginTop:"20px",marginBottom:"20px"}}>Bronze Sponsers</h4>  
+                           </div>
+                                       
+                                      
+                                           <div className="row row-img grid" style={{"justify-content": "center"}}>
+                                               {BronzeSponsers.map((item, index) => {
+                                               return (
+                                                   <div data-aos="fade-up" style={{paddingTop:"7%"}} className="col-md-4 col-sm-6 img-div">
+                                                       <a rel="noreferrer" href={item.sponsorLink} target="_blank">
+                                                           <img alt="" className="sponsor-img" src={item.sponsorImg} />
+                                                       </a>
+                                                   </div>                                                
+                                               )
+                                               })}
+                                           </div>
+                                       
+                        </div>
+                        </div>
+                </section>
+            </div>
+        </section>
+                    {/* <------------------------------Partners---------------> */}
+                    <section id="sponsors">
+                        {/* <div id="portfolio" className="our-portfolio section"> */}
+                  <div  className="container">
+                    <section className="sponsors-section">
+                  <div className="body-content sponsors-conatiner">
+                  <div className="community">
+                           
+                           <div className="section-heading wow fadeInLeft" data-wow-duration="1s" data-wow-delay="0.3s">
+                             
+                             <h4 class="sponsor-h4" style={{backgroundColor:"#003058",color:"#fff",textAlign:"center",marginTop:"20px",marginBottom:"20px"}}>Partners</h4>  
+                           </div>
+                                       
+                                      
+                                           <div className="row row-img grid" style={{"justify-content": "center"}}>
+                                               {Partners.map((item, index) => {
+                                               return (
+                                                   <div data-aos="fade-up" style={{paddingTop:"5%"}} className="col-md-4 col-sm-6 img-div">
+                                                       <a rel="noreferrer" href={item.sponsorLink} target="_blank">
+                                                           <img alt="" className="sponsor-img" src={item.sponsorImg} />
+                                                       </a>
+                                                   </div>                                                
+                                               )
+                                               })}
+                                           </div>
+                                       
+                        </div>
+                      
+                  </div></section></div></section>
+
+
+
+                  {/* <-----------------------------------------community partners----------------------------------> */}
+
+
+                    <section id="sponsors">
+                        {/* <div id="portfolio" className="our-portfolio section"> */}
+                  <div  className="container">
+                    <section className="sponsors-section">
+                  <div className="body-content sponsors-conatiner">
+                  <div className="community">
+                           
+                           <div className="section-heading wow fadeInLeft" data-wow-duration="1s" data-wow-delay="0.3s">
+                             
+                             <h4 class="sponsor-h4" style={{backgroundColor:"#696969",color:"#fff",textAlign:"center",marginTop:"20px",marginBottom:"20px"}}>Community Partners</h4>  
+                           </div>
+                                       
+                                      
+                                           <div className="row row-img grid" style={{"justify-content": "center"}}>
+                                               {CommunityPartners.map((item, index) => {
+                                               return (
+                                                   <div data-aos="fade-up" style={{paddingTop:"5%"}} className="col-md-4 col-sm-6 img-div">
+                                                       <a rel="noreferrer" href={item.sponsorLink} target="_blank">
+                                                           <img alt="" className="sponsor-img" src={item.sponsorImg} />
+                                                       </a>
+                                                   </div>                                                
+                                               )
+                                               })}
+                                           </div>
+                                       
+                        </div>
+                      
+                  </div></section></div></section>
+
+
+
+
+
+
+                    {/* <section className="sponsors-section">
+                    <div className="body-content sponsors-conatiner">
+                        <div style={{paddingTop:"2%"}}  className="community">
+                           
+                        <div className="section-heading wow fadeInLeft" data-wow-duration="1s" data-wow-delay="0.3s">
+                          
+                          <h4 class="sponsor-h4" style={{backgroundColor:"#003058",color:"#fff",textAlign:"center",marginTop:"20px",marginBottom:"20px"}}>Partners</h4>  
+                        </div>
+                                    <br />
+                                   
+                                        <div className="row row-img grid" style={{"justify-content": "center"}}>
+                                            {Partners.map((item, index) => {
+                                            return (
+                                                <div data-aos="fade-up" className="col-md-4 col-sm-6 img-div" style={{paddingTop:"5%"}}>
+                                                    <a href={item.sponsorLink} target="_blank">
+                                                        <img className="sponsor-img" src={item.sponsorImg} />
+                                                    </a>
+                                                </div>                                                
+                                            )
+                                            })}
+                                        </div>
+                                    
+                            </div>
+                            <div style={{paddingTop:"2%"}}  className="community">
+                           
+                        <div className="section-heading " data-wow-duration="1s" data-wow-delay="0.3s">
+                          
+                          <h4 class="sponsor-h4" style={{backgroundColor:"#696969",color:"#fff",textAlign:"center",marginTop:"20px",marginBottom:"20px"}}>Community Partners</h4>  
+                        </div>
+                                    <br />
+                                   
+                                        <div className="row row-img grid" style={{"justify-content": "center"}}>
+                                            {CommunityPartners.map((item, index) => {
+                                            return (
+                                                <div data-aos="fade-up" className="col-md-4 col-sm-6 img-div">
+                                                    <a href={item.sponsorLink} target="_blank">
+                                                        <img className="sponsor-img" src={item.sponsorImg} />
+                                                    </a>
+                                                </div>                                                
+                                            )
+                                            })}
+                                        </div>
+                                    
+                                </div>
+                    </div>
+                    </section> */}
 
 
 
                                         
                       
-                        <div style={{paddingTop:"8%"}}  className="community">
+                        {/* <div style={{paddingTop:"8%"}}  className="community">
                            
                         <div className="section-heading " data-wow-duration="1s" data-wow-delay="0.3s">
                           
@@ -66,7 +234,7 @@ function portfolio(){
                                             })}
                                         </div>
                                     
-                                </div>
+                                </div> */}
                     
                                 {/* <div className="col-md-6">
                                     <h3>Platform Partners</h3>
@@ -123,10 +291,6 @@ function portfolio(){
                                 >Sponsor Us
                             </a>
                             </div> */}
-                       
-                     </div>
-                </section>
-    </div></section>
 {/*  </div> */}
     </>)
 }
--- src/js/sponsersdata.js
+++ src/js/sponsersdata.js
@@ -1,4 +1,4 @@
-export const CommunityPartners = [
+export const CommunityPartners =[
     {
         sponsorLink:"https://http://rignitc.com/",
         sponsorImg:"assets/images/logos/RIG_NIT_Calicut.png"
@@ -10,6 +10,24 @@ export const CommunityPartners = [
     {
         sponsorLink:"http://rmi.nitt.edu/",
         sponsorImg:"assets/images/logos/RMI.png"
+    },
+]
+export const Partners=[
+    {
+        sponsorLink:"https://givemycertificate.com/",
+        sponsorImg:"assets/images/logos/gmc.png"
+    },
+    {
+        sponsorLink:"https://streamyard.com/",
+        sponsorImg:"assets/images/logos/StreamYard - Horz (2).png"
+    },
+    {
+        sponsorLink:"https://artofproblemsolving.com/",
+        sponsorImg:"assets/images/logos/AOPS.png"
+    },
+    {
+        sponsorLink:"https://www.myinterviewbuddy.com/",
+        sponsorImg:"assets/images/logos/Interview_buddy.png"
     },
 ]
 
@@ -33,14 +51,6 @@ export const EduPartners = [
         sponsorLink:"https://givemycertificate.com/",
         sponsorImg:"assets/images/logos/gmc.png"
     },
-    {
-        sponsorLink:"https://hoverrobotix.com/",
-        sponsorImg:"assets/images/logos/HoverRobotix_Logo_round_clear_png.png"
-    },
-    {
-        sponsorLink:"https://thementorx.com/",
-        sponsorImg:"assets/images/logos/MENTORx_logo_black_with_white_tag_line_-_Round.png"
-    },
     
     {
         sponsorLink:"https://streamyard.com/",
@@ -60,12 +70,47 @@ export const EduPartners = [
     },
     
     {
-        sponsorLink:"https://www.leading-learners.com/",
-        sponsorImg:"assets/images/logos/Leading_Learners_Logo.png"
-    },
-    {
         sponsorLink:"https://artofproblemsolving.com/",
         sponsorImg:"assets/images/logos/AOPS.png"
     },
    
+]
+export const GoldSponsors = [
+    {
+        sponsorLink:"https://hoverrobotix.com/",
+        sponsorImg:"assets/images/logos/HoverRobotix_Logo_round_clear_png.png"
+    },
+    {
+        sponsorLink:"https://thementorx.com/",
+        sponsorImg:"assets/images/logos/MENTORx_logo_black_with_white_tag_line_-_Round.png"
+    },
+    {
+        sponsorLink:"https://www.leading-learners.com/",
+        sponsorImg:"assets/images/logos/Leading_Learners_Logo.png"
+    },
+]
+export const SilverSponsers = [
+    {
+        sponsorLink: "http://rosenfeldmedia.com/",
+        sponsorImg: "/assets/images/logos/Rosenfeld-min.png",
+    },
+    {
+        sponsorLink:"https://www.taskade.com/",
+        sponsorImg:"assets/images/logos/taskade-circle-logo-full-black.png"
+    },
+    {
+        sponsorLink:"https://www.interviewcake.com/",
+        sponsorImg:"assets/images/logos/cake_logo_white_on_blue.svg"
+    },
+]
+export const BronzeSponsers = [
+    {
+        sponsorLink:"https://www.stickermule.com/en-in",
+        sponsorImg:"assets/images/logos/02-sticker-mule-logo-light.png"
+    },
+    {
+        sponsorLink:"https://www.axure.com/",
+        sponsorImg:"assets/images/logos/axurelogo.png"
+    },
+    
 ]
