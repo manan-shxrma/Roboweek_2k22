4fd5bab00b8ed16080c238afe786ff1a088cce3b
manan-shxrma <manansharmams3@gmail.com>
2022-02-23 12:20:47 +0530

    buttons

--- src/components/topsection.js
+++ src/components/topsection.js
@@ -8,7 +8,12 @@ import Illustration from "./illustration";
 
 AOS.init();
 function Topsection(){
-
+function activateLasers(){
+  window.location.href = "https://dare2compete.com/festival/roboweek-20-national-institute-of-technology-nit-hamirpur-20715";
+}
+function activateLasers1(){
+  window.location.href = "https://discord.com/invite/vHz3tyjyke";
+}
 return(<>
   
   <section id="home" class="background">
@@ -16,8 +21,8 @@ return(<>
   <div data-aos="fade-right" className="headingdiv"> 
   <h1 class="hero-title">RoboWeek 2.0</h1>
   <h5 className="underhero">Let's build the future</h5>
- <a href="https://dare2compete.com/festival/roboweek-20-national-institute-of-technology-nit-hamirpur-20715"> <button id="bottone15"><i style={{paddingTop:"1%",paddingRight:"3%"}} class="fas fa-award" /> Register</button></a>
- <a href="https://discord.gg/vHz3tyjyke"> <button id="bottone15"><i style={{paddingTop:"1%",paddingRight:"3%"}} className="fab fa-discord" /> Discord</button> </a>
+ <button onClick={activateLasers} id="bottone15"><i style={{paddingTop:"1%",paddingRight:"3%"}} class="fas fa-award" /> Register</button>
+ <button style={{marginLeft:"3%"}} onClick={activateLasers1} id="bottone15"><i style={{paddingTop:"1%",paddingRight:"3%"}} className="fab fa-discord" /> Discord</button> 
  <div id="timer">
   <span id="days"></span>days
   <span id="hours"></span>hours
