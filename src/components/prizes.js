0d582db2900299f462835c0d2f8f53bf7b6c5a46
Aakash Tiwari <70657103+Aakash872@users.noreply.github.com>
2022-03-16 01:11:52 +0530

    Update prizes.js

--- src/components/prizes.js
+++ src/components/prizes.js
@@ -168,8 +168,8 @@ function prizes(){
          <h3 style={{color:"#fff", textAlign:"center",fontSize:"4rem"}}>Callisto</h3>
    <div style={{paddingTop:"3%"}} class='pricing pricing-palden'>
     <div  class='pricing-item'>
-      <div style={{background:"#CD7F32"}} class='pricing-deco'>
-       <img className="medalImg" alt="medal1"  src="assets/images/3rd.svg"/> 
+      <div style={{background:"#CD7F32", paddingBottom:"200px"}} class='pricing-deco'>
+      <img className="medalImg" alt="medal1"  src="assets/images/3rd.svg"/>
         <svg class='pricing-deco-img' enable-background='new 0 0 300 100' height='100px' id='Layer_1' preserveAspectRatio='none' version='1.1' viewBox='0 0 300 100' width='300px' x='0px' xmlSpace='preserve' xmlnsXlink='http://www.w3.org/1999/xlink' xmlns='http://www.w3.org/2000/svg' y='0px'>
           <path class='deco-layer deco-layer--1' d='M30.913,43.944c0,0,42.911-34.464,87.51-14.191c77.31,35.14,113.304-1.952,146.638-4.729&#x000A;	c48.654-4.056,69.94,16.218,69.94,16.218v54.396H30.913V43.944z' fill='#FFFFFF' opacity='0.6'></path>
           <path class='deco-layer deco-layer--2' d='M-35.667,44.628c0,0,42.91-34.463,87.51-14.191c77.31,35.141,113.304-1.952,146.639-4.729&#x000A;	c48.653-4.055,69.939,16.218,69.939,16.218v54.396H-35.667V44.628z' fill='#FFFFFF' opacity='0.6'></path>
@@ -177,16 +177,17 @@ function prizes(){
           <path class='deco-layer deco-layer--4' d='M-34.667,62.998c0,0,56-45.667,120.316-27.839C167.484,57.842,197,41.332,232.286,30.428&#x000A;	c53.07-16.399,104.047,36.903,104.047,36.903l1.333,36.667l-372-2.954L-34.667,62.998z' fill='#FFFFFF'></path>
         </svg>
       </div>
+      <div class="third-prize">
       <h2>2nd Runners Up</h2>
       <ul class='pricing-feature-list' style={{paddingLeft:"20px", paddingRight:"20px"}}>
         <li class='pricing-feature'>Gift vouchers from HoverRobotix Hoverboards India worth Rs 10000/- each</li>
         <li class='pricing-feature'>3 months free hosting.</li>
         <li class='pricing-feature'>Async online mock interviews with unlimited practice</li>
       </ul>
-      
+      </div>
     </div>
     <div class='pricing-item pricing__item--featured'>
-      <div style={{background:"#DAA520",height:"50%"}} class='pricing-deco'>
+      <div style={{background:"#DAA520", paddingBottom:"200px"}} class='pricing-deco'>
       <img className="medalImg" alt="medal1"  src="assets/images/1st.svg"/>
         <svg class='pricing-deco-img' enable-background='new 0 0 300 100' height='10px' id='Layer_1' preserveAspectRatio='none' version='1.1' viewBox='0 0 300 100' width='300px' x='0px' xmlSpace='preserve' xmlnsXlink='http://www.w3.org/1999/xlink' xmlns='http://www.w3.org/2000/svg' y='0px'>
           <path class='deco-layer deco-layer--1' d='M30.913,43.944c0,0,42.911-34.464,87.51-14.191c77.31,35.14,113.304-1.952,146.638-4.729&#x000A;	c48.654-4.056,69.94,16.218,69.94,16.218v54.396H30.913V43.944z' fill='#FFFFFF' opacity='0.6'></path>
@@ -196,16 +197,18 @@ function prizes(){
         </svg>
       </div>
       <div>
+        <div class="first-prize">
       <h2>Winners</h2>
-      <ul class='pricing-feature-list' style={{paddingLeft:"20px", paddingRight:"20px"}}>
+      <ul class='pricing-feature-list' style={{paddingLeft:"20px", paddingRight:"20px", paddingBottom:"80px"}}>
         <li class='pricing-feature'>Free D2C Pro membership worth Rs. 999, giving them free access to high quality courses</li>
         <li class='pricing-feature'>12 months free hosting</li>
         <li class='pricing-feature'>Async online mock interviews with unlimited practice</li>
       </ul></div>
+      </div>
       
     </div>
     <div class='pricing-item'>
-      <div style={{background:"#A9A9A9" }} class='pricing-deco'>
+      <div style={{background:"#A9A9A9", paddingBottom:"200px" }} class='pricing-deco'>
       <img className="medalImg" alt="medal1"  src="assets/images/2nd.svg"/>
         <svg class='pricing-deco-img' enable-background='new 0 0 300 100' height='100px' id='Layer_1' preserveAspectRatio='none' version='1.1' viewBox='0 0 300 100' width='300px' x='0px' xmlSpace='preserve' xmlnsXlink='http://www.w3.org/1999/xlink' xmlns='http://www.w3.org/2000/svg' y='0px'>
           <path class='deco-layer deco-layer--1' d='M30.913,43.944c0,0,42.911-34.464,87.51-14.191c77.31,35.14,113.304-1.952,146.638-4.729&#x000A;	c48.654-4.056,69.94,16.218,69.94,16.218v54.396H30.913V43.944z' fill='#FFFFFF' opacity='0.6'></path>
@@ -214,12 +217,14 @@ function prizes(){
           <path class='deco-layer deco-layer--4' d='M-34.667,62.998c0,0,56-45.667,120.316-27.839C167.484,57.842,197,41.332,232.286,30.428&#x000A;	c53.07-16.399,104.047,36.903,104.047,36.903l1.333,36.667l-372-2.954L-34.667,62.998z' fill='#FFFFFF'></path>
         </svg>
       </div>
+      <div class="second-prize">
       <h2>Runners Up</h2>
       <ul class='pricing-feature-list' style={{paddingLeft:"20px", paddingRight:"20px"}}>
         <li class='pricing-feature'>Gift vouchers from HoverRobotix Hoverboards India worth Rs 10000/- each</li>
         <li class='pricing-feature'>6 months free hosting</li>
         <li class='pricing-feature'>Async online mock interviews with unlimited practice</li>
       </ul>
+      </div>
      </div> 
     </div>
   </div>
